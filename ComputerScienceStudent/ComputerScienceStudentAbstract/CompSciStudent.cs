74c78ea3a2c0e7cad844eb2050503f2cc63eb281
Trace_Code89 <71043578+TraceyP89@users.noreply.github.com>
2023-07-05 21:19:54 -0400

    Add files via upload

---
+++ ComputerScienceStudent/ComputerScienceStudentAbstract/CompSciStudent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerScienceStudentAbstract
+{
+     class CompSciStudent : Student
+    {
+        // Constants to set the values for the required hours
+        private double MATH_HOURS = 20;
+        private double CS_HOURS = 40;
+        private double GEN_HOURS = 60;
+
+        //Fields
+        private string _academicTrack;
+
+        //Constructor
+        public CompSciStudent(string name, string id, string track)
+           : base(name, id)
+        {
+            AcademicTrack = track; 
+        }
+        //Get and set the data for the academic track
+        public string AcademicTrack { get; set; }
+
+        //this method will get the total hours required. This is read-only
+        public override double RequiredHours
+        {
+            get { return MATH_HOURS + CS_HOURS + GEN_HOURS; }
+        }
+    }
+}
---
+++ ComputerScienceStudent/ComputerScienceStudentAbstract/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerScienceStudentAbstract
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getRequiredHoursButton_Click(object sender, EventArgs e)
+        {
+            //Variable that will hold the values for name, id and track
+            string name, id, track;
+
+            //Get student's name and ID.
+            name = nameTextBox.Text;    
+            id = idTextBox.Text;
+
+            //Get student's academic track.
+            if (infoSystemsRadioButton.Checked)
+            {
+                track = "Information System";
+            }
+            else
+            {
+                track = "Software Engineering";
+            }
+
+            //Create a CompSciStudent object
+            CompSciStudent csStudent = new CompSciStudent(name, id, track);
+
+            //Display student's reuired hours
+            studentsNameLabel.Text = csStudent.Name;
+            studentsIDLabel.Text = csStudent.ID;
+            requiredHoursLabel.Text = csStudent.RequiredHours.ToString("n1");
+        }
+
+        //this closes the program
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
